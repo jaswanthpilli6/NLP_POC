b7944c2a9ddeac2e881e8445e0ee0adce0dc23cf
guo feng <guofeng20094@gmail.com>
2019-07-29 00:52:44 +0800

    updated

--- poc.py
+++ poc.py
@@ -1,6 +1,7 @@
 # %%
 from bs4 import BeautifulSoup
 import requests
+import re
 
 # https://www.datacamp.com/community/tutorials/text-analytics-beginners-nltk
 
@@ -10,12 +11,53 @@ html_tags = BeautifulSoup(the_page, features="html.parser")
 
 paragraphs = html_tags.findAll('p', {'class': ''})
 
-p2_text = paragraphs[1].text
+article_text = ''
+for p in paragraphs:
+    article_text += ' ' + p.text
+
+# Removing Square Brackets and Extra Spaces
+article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+article_text = re.sub(r'\s+', ' ', article_text)
+
+# Removing special characters and digits
+formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
 # %%
-# import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
+import nltk
 
-tokens = word_tokenize(p2_text)
-print(tokens)
+sentence_list = sent_tokenize(article_text)
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_frequencies = {}
+for word in word_tokenize(formatted_article_text):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+maximum_frequncy = max(word_frequencies.values())
+
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word] / maximum_frequncy)
+
+sentence_scores = {}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+# %%
+import heapq
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+print(summary)
