289b6b839b157395d81e080c787cbd8553bf54a9
guo feng <guofeng20094@gmail.com>
2019-11-19 23:11:37 +0800

    update

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,17 +1,30 @@
 import PyPDF2
+import re
 
-pdf_file = open('AR_2017.pdf', 'rb')
-read_pdf = PyPDF2.PdfFileReader(pdf_file)
-number_of_pages = read_pdf.getNumPages()
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tag import pos_tag
 
-print(number_of_pages)
+import heapq
 
-text = ''
-for i in range(number_of_pages):
-    page_content = read_pdf.getPage(i).extractText()
-    print(page_content)
-    text = text + ' '.join(page_content.split()) + ' '
-    print('------------------------------------------')
+import json
+import pkg_resources
+
+from symspellpy.symspellpy import SymSpell
+
+KEY_RATIOS = ['sales', 'revenue', 'operating expense', 'gross margin', 'operating margin', 'gearing', 'ebitda', 'net profit']
+
+KEY_PHRASE = ['due to', 'because', 'as a result', 'impacted by']
+
+def read_full_pdf(fname):
+    pdfFileObj = open(fname, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    print("Total pages in file", pdfReader.numPages)
+    full_text = ''
+    for i in range(pdfReader.numPages):
+        text = pdfReader.getPage(i).extractText()
+        tmp = ' '.join(text.split())
 
 
 # print(text)
